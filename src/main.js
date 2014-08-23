91cd3afe20df72376b5ceb6ad358d18e4af36033
Graham Cox <graham@grahamcox.co.uk>
2014-08-23 18:19:06 +0000

    If the PORT environment variable is set then listen on that, otherwise pick a random port

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 var restify = require("restify"),
     url = require("url");
 
+var port = process.env.PORT || 0;
+
 var server = restify.createServer({
     name: "RemoteSpeakers"
 });
 
-server.listen(0, function() {
+server.listen(process.env.PORT, function() {
     var parsedUrl = url.parse(server.url);
     console.log("Server %s listening at %s", server.name, parsedUrl.port);
 });
