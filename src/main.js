cd858dcfb02a3c639eb51cf3c038b0c00a259862
Graham Cox <graham@grahamcox.co.uk>
2014-08-23 18:23:40 +0000

    Route to get the speaker name, with hardcoded result

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ var server = restify.createServer({
     name: "RemoteSpeakers"
 });
 
+server.get("/config/name", function(req, res, next) {
+    res.send("Master Bedroom");
+    next();
+});
+
 server.listen(process.env.PORT, function() {
     var parsedUrl = url.parse(server.url);
     console.log("Server %s listening at %s", server.name, parsedUrl.port);
